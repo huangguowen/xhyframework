8589e60a180700036e22f8ed04b1dfd56bb73283
huangguowen <1194729753@qq.com>
2020-07-21 14:59:59 +0800

    x

--- framework/middleware/AuthTokenMiddleware.php
+++ framework/middleware/AuthTokenMiddleware.php
@@ -14,21 +14,33 @@ class AuthTokenMiddleware extends Middleware
 {
     public function handle($request, \Closure $next)
     {
-       try {
-          JWTAuth::auth();
-       } catch (\Exception $e) {
-           if ($e instanceof TokenExpiredException) {
-               return XhyResponse::fail("token 过期");
-           }
-           if ($e instanceof TokenBlacklistException) {
-               return XhyResponse::fail("您已下线");
-           }
-           if ($e instanceof TokenInvalidException) {
-               return XhyResponse::fail("token 不合法");
-           }
-           return XhyResponse::fail("登录用户不合法");
-       }
+        try {
+            JWTAuth::auth();
+        } catch (\Exception $e) {
+            if ($e instanceof TokenExpiredException) {
+                return json([
+                    'code' => 10005,
+                    'message'  => 'The token is expired.',
+                ]);
+            }
+            if ($e instanceof TokenBlacklistException) {
+                return json([
+                    'code' => 10005,
+                    'message'  => 'The token is in blacklist.',
+                ]);
+            }
+            if ($e instanceof TokenInvalidException) {
+                return json([
+                    'code' => 10006,
+                    'message'  => 'Token Signature could not be verified.',
+                ]);
+            }
+            return json([
+                'code' => 10005,
+                'message'  => '登录用户不合法',
+            ]);
+        }
 
-       return $next($request);
+        return $next($request);
     }
 }
