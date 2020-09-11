26914fbbb6b5216a855c687f1fe9214d54eca426
huangguowen <1194729753@qq.com>
2020-09-11 16:09:51 +0800

    x

--- framework/XhyAdminService.php
+++ framework/XhyAdminService.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * name: 系统服务
+ * date: 2020-09-019
+ * user: 黄国文
+ */
 namespace xhyadminframework;
 use think\exception\Handle;
 use think\facade\Validate;
@@ -9,21 +14,21 @@ use xhyadminframework\XhyQuery;
 class XhyAdminService extends Service
 {
     /**
-     *
-     * @time 2019年11月29日
-     * @return void
+     * notes: 注册通用命令
+     * author: 黄国文
+     * Date: 2020/9/8 0009
+     * Time: 下午 3:24
      */
     public function boot()
     {
+        //方法是在所有的系统服务注册完成之后调用，用于定义启动某个系统服务之前需要做的操作
     }
 
     /**
-     * register
-     *
-     * @author JaguarJack
-     * @email njphper@gmail.com
-     * @time 2020/1/30
-     * @return void
+     * notes: 注册通用命令
+     * author: 黄国文
+     * Date: 2020/9/8 0009
+     * Time: 下午 4:24
      */
     public function register()
     {
@@ -36,20 +41,24 @@ class XhyAdminService extends Service
     }
 
     /**
-     *
-     * @time 2019年12月13日
-     * @return void
+     * notes: 注册通用命令
+     * author: 黄国文
+     * Date: 2020/9/8 0009
+     * Time: 下午 4:24
      */
     protected function registerCommands(): void
     {
+        $commands = config('xhy.command');
         $this->commands([
-
+            $commands
         ]);
     }
+
     /**
-     *
-     * @time 2019年12月07日
-     * @return void
+     * notes: 注册验证器
+     * author: 黄国文
+     * Date: 2020/9/9 0009
+     * Time: 下午 2:20
      */
     protected function registerValidates(): void
     {
@@ -64,20 +73,21 @@ class XhyAdminService extends Service
     }
 
     /**
-     *
-     * @time 2019年12月12日
-     * @return void
+     * notes: 注册中间件
+     * author: 黄国文
+     * Date: 2020/9/9 0009
+     * Time: 下午 2:24
      */
     protected function registerMiddleWares(): void
     {
-        // todo
+//        $this->app->middleware->add(config('xhy.middleware'));
     }
 
     /**
-     * 注册监听者
-     *
-     * @time 2019年12月12日
-     * @return void
+     * notes: 注册监听者
+     * author: 黄国文
+     * Date: 2020/9/9 0009
+     * Time: 下午 2:25
      */
     protected function registerEvents(): void
     {
@@ -85,10 +95,10 @@ class XhyAdminService extends Service
     }
 
     /**
-     * register query
-     *
-     * @time 2020年02月20日
-     * @return void
+     * notes: 注册 query
+     * author: 黄国文
+     * Date: 2020/9/9 0009
+     * Time: 下午 2:25
      */
     protected function registerQuery(): void
     {
@@ -102,10 +112,10 @@ class XhyAdminService extends Service
     }
 
     /**
-     * register exception
-     *
-     * @time 2020年02月20日
-     * @return void
+     * notes: 注册 异常类
+     * author: 黄国文
+     * Date: 2020/9/9 0009
+     * Time: 下午 3:25
      */
     protected function registerExceptionHandle(): void
     {
