2769f4bbac013431b8e8007ab207c041fd1de239
1194729753@qq.com <1194729753@qq.com>
2020-12-21 17:30:28 +0800

    no message

--- framework/notAuthRoute.php
+++ framework/notAuthRoute.php
@@ -1,4 +1,4 @@
 <?php
-$router->post('login', '\xhyadminframework\controller\Common@login'); //核心登录
+$router->post('login', '\xhyadminframework\controller\Common@login'); //核心登录test
 $router->post('common/logout', '\xhyadminframework\controller\Common@logout'); //登出
 
