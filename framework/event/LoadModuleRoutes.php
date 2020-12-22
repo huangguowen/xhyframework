81afab8f1dec7d8f2ef72487573286d9a1f2b331
1194729753@qq.com <1194729753@qq.com>
2020-12-22 15:48:53 +0800

    核心更新 路由加载新增二级域名配置

--- framework/event/LoadModuleRoutes.php
+++ framework/event/LoadModuleRoutes.php
@@ -18,7 +18,7 @@ class LoadModuleRoutes
     {
         $router = app(Route::class);
 
-        $domain = config('xhy.domain');
+        $domain = config('xhy.domain') ?? '';
 
         $routes = XhyAdmin::getRoutes();
         if (!empty(\think\facade\Env::get('appconfig.APPNAME'))) {
@@ -30,40 +30,40 @@ class LoadModuleRoutes
         $routeMiddleware = config('xhy.route_middleware');
         //config配置项
         $configMiddleware = [\xhyadminframework\middleware\Config::class];
-        $router->group(function () use ($router, $routes) {
+        $router->group($domain, function () use ($router, $routes) {
             foreach ([$routes[0], @$routes[2]] as $route) {
                 include $route;
             }
         })->middleware(array_merge($routeMiddleware, $configMiddleware));
 
         //framework的未登录
-        $router->group(function () use ($router, $routes) {
+        $router->group($domain, function () use ($router, $routes) {
             include $routes[1];
         })->middleware($configMiddleware);
 
         // app加载登录
         if (!empty(\think\facade\Env::get('appconfig.APPNAME'))) {
             //不需要登录的模块
-            $router->group(function () use ($router) {
+            $router->group($domain, function () use ($router) {
                 include app()->getRootPath() . \think\facade\Env::get('appconfig.APPNAME') . '/noMiddlewareRoute.php';
             })->middleware($configMiddleware);
         }
         // 做游客登录需要用到 游客登录的 路由名称是 vistorRoute.php (在路由加中间件 易于管理)
         if (file_exists(app()->getRootPath() . \think\facade\Env::get('appconfig.APPNAME') . '/vistorRoute.php')) {
-            $router->group(function () use ($router) {
+            $router->group($domain, function () use ($router) {
                 include app()->getRootPath() . \think\facade\Env::get('appconfig.APPNAME') . '/vistorRoute.php';
             });
         }
         // 合并app 和 admin
         if (file_exists(app()->getRootPath() . \think\facade\Env::get('appconfig.APPNAME2') . '/noMiddlewareRoute.php')) {
             //不需要登录的模块
-            $router->group(function () use ($router) {
+            $router->group($domain, function () use ($router) {
                 include app()->getRootPath() . \think\facade\Env::get('appconfig.APPNAME2') . '/noMiddlewareRoute.php';
             })->middleware($configMiddleware);
         }
         // 合并app 和 admin
         if (!empty(\think\facade\Env::get('appconfig.APPNAME2'))) {
-            $router->group(function () use ($router, $routes) {
+            $router->group($domain, function () use ($router, $routes) {
                 include $routes[3];
             })->middleware(['xhyadminframework\middleware\AuthTokenMiddleware']);
         }
